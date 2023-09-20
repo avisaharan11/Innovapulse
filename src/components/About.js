eae7b76e09ead0ad1d3232c4479840550043245c
Abhimanu Saharan <avisaharan1@gmail.com>
2023-09-20 18:50:16 +0530

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div className="about">
             <h2>About Us</h2>
-            <p>Transforming the global freelance marketplace by connecting clients with pre-vetted global talent, backed by instant job payments and least documentation.</p>
+            <p>Redefining the freelance landscape by effortlessly connecting you with top global talent, all backed by instant payments and streamlined paperwork.</p>
         </div>
     );
 };
