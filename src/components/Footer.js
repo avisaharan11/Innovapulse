bbb616d779e48de559f3a288880047e64e197911
Abhimanu Saharan <avisaharan1@gmail.com>
2023-09-20 11:38:33 +0530

    added address and phone

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Contact Us: info@gignance.com</p>
+            <p>Contact Us: info@gignance.com or +64225401142</p>
+            <p>Address: 222, Waimea Road, Bishopdale, Nelson, NZ 7011</p>
         </div>
     );
 };
